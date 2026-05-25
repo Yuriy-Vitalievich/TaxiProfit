387ac787b5eb004ad5086511c47a89c3171ccdd7
Юра <ura@MacBook-Pro-Ura.local>
2026-05-26 01:30:02 +0300

    Refine driver profile cabinet

--- service-worker.js
+++ service-worker.js
@@ -1,10 +1,10 @@
-const CACHE_NAME = "taxiprofit-telegram-20260526-1";
+const CACHE_NAME = "taxiprofit-telegram-20260526-2";
 const APP_SHELL = [
   "./",
   "./index.html",
-  "./styles.css?v=telegram-20260526-1",
-  "./app.js?v=telegram-20260526-1",
-  "./manifest.webmanifest?v=telegram-20260526-1",
+  "./styles.css?v=telegram-20260526-2",
+  "./app.js?v=telegram-20260526-2",
+  "./manifest.webmanifest?v=telegram-20260526-2",
   "./assets/icons/taxiprofit-icon.svg",
 ];
 
