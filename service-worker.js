a58ec50d7d50aa5207a8b16ac66f38c6bb69cf1f
Юра <ura@MacBook-Pro-Ura.local>
2026-05-26 01:44:54 +0300

    Fix profile edit toggle placement

--- service-worker.js
+++ service-worker.js
@@ -1,10 +1,10 @@
-const CACHE_NAME = "taxiprofit-telegram-20260526-2";
+const CACHE_NAME = "taxiprofit-telegram-20260526-3";
 const APP_SHELL = [
   "./",
   "./index.html",
-  "./styles.css?v=telegram-20260526-2",
-  "./app.js?v=telegram-20260526-2",
-  "./manifest.webmanifest?v=telegram-20260526-2",
+  "./styles.css?v=telegram-20260526-3",
+  "./app.js?v=telegram-20260526-3",
+  "./manifest.webmanifest?v=telegram-20260526-3",
   "./assets/icons/taxiprofit-icon.svg",
 ];
 
