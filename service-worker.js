b83767626fa7fbe546832d7daf4c43483d6149eb
Юра <ura@MacBook-Pro-Ura.local>
2026-05-27 22:01:53 +0300

    Add taxi economy mileage and fuel logic

--- service-worker.js
+++ service-worker.js
@@ -1,10 +1,10 @@
-const CACHE_NAME = "taxiprofit-telegram-20260527-4";
+const CACHE_NAME = "taxiprofit-telegram-20260527-5";
 const APP_SHELL = [
   "./",
   "./index.html",
-  "./styles.css?v=telegram-20260527-4",
-  "./app.js?v=telegram-20260527-4",
-  "./manifest.webmanifest?v=telegram-20260527-4",
+  "./styles.css?v=telegram-20260527-5",
+  "./app.js?v=telegram-20260527-5",
+  "./manifest.webmanifest?v=telegram-20260527-5",
   "./assets/icons/taxiprofit-icon.svg",
 ];
 
